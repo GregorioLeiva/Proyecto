93462c86886aa9a85d03081f10a802025b1f60c3
GregorioLeiva <gleiva@frsn.utn.edu.ar>
2024-06-26 14:34:12 -0300

    Estructura realizada, ver recomendaciones y dar estilo

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Obtener el parámetro de la URL
+  // sacamos el parametro de comida
   const urlParams = new URLSearchParams(window.location.search);
   const food = urlParams.get('food');
   const TU_API_KEY = '42125001a1334f56aa22b7d4b7f27732';
 
-  // Consultar la API de Spoonacular para obtener recetas
+  // consulta a la api de spoonacular
   fetch(`https://api.spoonacular.com/recipes/search?query=${food}&number=10&apiKey=${TU_API_KEY}`)
     .then(response => response.json())
     .then(data => {
-      // Manipular los datos de las recetas obtenidas
+      // manipulacion de datos
       const recipesContainer = document.getElementById('recipes-container');
 
       data.results.forEach(recipe => {
-        // Crear elementos HTML para mostrar cada receta
+        // creamos los elementos del html
         const recipeCard = document.createElement('div');
         recipeCard.classList.add('recipe-card');
-        recipeCard.innerHTML = `
-          <h3>${recipe.title}</h3>
-          <p>${recipe.summary}</p>
-        `;
-        recipesContainer.appendChild(recipeCard);
+
+        // Hacer una solicitud adicional para obtener detalles de la receta
+        fetch(`https://api.spoonacular.com/recipes/${recipe.id}/information?apiKey=${TU_API_KEY}`)
+          .then(response => response.json())
+          .then(details => {
+            recipeCard.innerHTML = `
+              <h3>${details.title}</h3>
+              <img src="${details.image}" alt="${details.title}" />
+              <p>${details.summary}</p>
+            `;
+            recipesContainer.appendChild(recipeCard);
+          })
+          .catch(error => {
+            console.error('Error al recuperar detalles de la receta:', error);
+          });
       });
     })
     .catch(error => {
-      console.error('Error fetching recipes:', error);
+      console.error('Error al recuperar Recetas:', error);
     });
 });
